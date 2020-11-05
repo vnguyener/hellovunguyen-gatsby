212095ee38339a72e69705c5590a35247e8a8fcd
Vu Nguyen <hello.vunguyen@gmail.com>
2020-11-04 21:31:33 -0600

    editing gatsby config for title, adding favicon, removing default gatsby images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hello, Vu`,
+    description: `Software Engineering Portfolio`,
+    author: `Vu Nguyen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `hello-vunguyen-gatsby`,
+        short_name: `hello-vu`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
